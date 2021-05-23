add714d38097895bf9996cbda2cc11405ec2540f
Luong <LuongTran1421@gmail.com>
2021-05-23 22:54:39 +0700

    All project done.

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,3 @@
-import "../scss/main.scss";
-
-import "../images/Icon_mail.svg";
-import "../images/Icon_web.svg";
-import "../images/About_image.svg";
-import "../images/Hero_bg.png";
-import "../images/Hero_image.png";
-import "../images/Logo.png";
-import "../images/Portfolio_bg.png";
-import "../images/slide1.jpg";
-import "../images/slide2.jpg";
-import "../images/slide3.jpg";
-import "../images/slide4.jpg";
-import "../images/slide5.jpg";
-
 const showMenu = () => {
 	const btnOpen = document.querySelector(".open__bar");
 	const btnClose = document.querySelector(".close__bar");
