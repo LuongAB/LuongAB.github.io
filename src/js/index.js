e5087007232582de976428cf0626fa71bfc3f3a6
Luong <LuongTran1421@gmail.com>
2021-05-23 21:52:27 +0700

    Project done.

---
+++ src/js/index.js
@@ -0,0 +1,95 @@
+import "../scss/main.scss";
+
+import "../images/Icon_mail.svg";
+import "../images/Icon_web.svg";
+import "../images/About_image.svg";
+import "../images/Hero_bg.png";
+import "../images/Hero_image.png";
+import "../images/Logo.png";
+import "../images/Portfolio_bg.png";
+import "../images/slide1.jpg";
+import "../images/slide2.jpg";
+import "../images/slide3.jpg";
+import "../images/slide4.jpg";
+import "../images/slide5.jpg";
+
+new WOW().init();
+
+const showMenu = () => {
+	const btnOpen = document.querySelector(".open__bar");
+	const btnClose = document.querySelector(".close__bar");
+	const link = document.querySelectorAll(".menu__left a");
+	const menu = document.querySelector(".menu");
+
+	btnOpen.addEventListener("click", () => {
+		menu.classList.add("show__menu");
+	});
+
+	btnClose.addEventListener("click", () => {
+		menu.classList.remove("show__menu");
+	});
+
+	link.forEach(item =>
+		item.addEventListener("click", () => {
+			menu.classList.remove("show__menu");
+		})
+	);
+	window.addEventListener("click", event => {
+		if (event.target == menu) {
+			menu.classList.remove("show__menu");
+		}
+	});
+};
+showMenu();
+
+$(".gallery").slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	dots: false,
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 2500,
+	mobileFirst: true,
+	responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			},
+		},
+		{
+			breakpoint: 1023,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			},
+		},
+	],
+});
+
+$(".bloglist").slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	arrows: false,
+	dots: false,
+	mobileFirst: true,
+	autoplaySpeed: 3000,
+	responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			},
+		},
+		{
+			breakpoint: 1023,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			},
+		},
+	],
+});
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/js/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].js",
+		assetModuleFilename: "images/[name][ext]",
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader",
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.js$/i,
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.(png|jpg|svg)$/i,
+				type: "asset",
+			},
+		],
+	},
+	devtool: "source-map",
+	plugins: [
+		new HtmlWebPackPlugin({
+			title: "Doob",
+			inject: "body",
+			template: "./src/index.html",
+		}),
+		new MiniCssExtractPlugin(),
+	],
+};
